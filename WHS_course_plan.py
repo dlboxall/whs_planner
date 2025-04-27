e8710d2c5e2b93cdc6cdc4f3cf546159ef4861a5
dlboxall <dlboxall8@gmail.com>
2025-04-27 16:07:45 -0500

    Update WHS_course_plan.py

--- WHS_course_plan.py
+++ WHS_course_plan.py
@@ -27,6 +27,13 @@ course_catalog = load_course_catalog()
 # --- Helper function to check prerequisites ---
 def has_prereq_met(course_code, current_year, course_plan_codes, prereq_dict):
     taken = []
+
+    # First, add Middle School credits to taken list!
+    if "ms_credits" in st.session_state:
+        ms_taken = st.session_state.ms_credits
+        taken += [course_catalog.loc[course_catalog["Course Name"] == name, "Course Code"].values[0]
+                  for name in ms_taken if name.strip() != ""]
+    
     for yr in years:
         if years.index(yr) >= years.index(current_year):
             break
