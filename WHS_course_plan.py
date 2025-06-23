68888a95a51a6ad946d7a5854d927150270ce363
dlboxall <dlboxall8@gmail.com>
2025-06-23 14:15:28 -0500

    Update WHS_course_plan.py

--- WHS_course_plan.py
+++ WHS_course_plan.py
@@ -21,10 +21,10 @@ dept_code_to_name = {
 
 st.set_page_config(page_title="WHS Course Planner", layout="wide")
 
-col1, col2, col3 = st.columns([1, 3, 2])
+col1, col2, col3 = st.columns([2, 3, 2])
 
 with col1:
-    st.image("WHS_logo2.webp", width=100)
+    st.image("WHS_logo2.webp", width=120)
 
 with col2:
     st.markdown("<h1 style='text-align: center;'>WHS Course Planner Dashboard</h1>", unsafe_allow_html=True)
